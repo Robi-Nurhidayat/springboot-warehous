cfb27009dee09efcb35e6c1820864574459af109
Robi Nurhidayat <robivanpersie290899@gmail.com>
2024-08-21 08:48:05 +0700

    add field in user

--- inventory/src/main/java/com/inventory/inventory/entity/User.java
+++ inventory/src/main/java/com/inventory/inventory/entity/User.java
@@ -1,15 +1,23 @@
 package com.inventory.inventory.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "users")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String role;
 
 }
