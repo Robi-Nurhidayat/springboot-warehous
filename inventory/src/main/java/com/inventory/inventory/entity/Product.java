dca5b8be703ea7ed4353f2d12a2024fc005d3c09
Robi Nurhidayat <robivanpersie290899@gmail.com>
2024-08-21 07:41:25 +0700

    error test di postman saat create product, menambahkan @Tabel dan @generatedValue pada entity baru solved

--- inventory/src/main/java/com/inventory/inventory/entity/Product.java
+++ inventory/src/main/java/com/inventory/inventory/entity/Product.java
@@ -1,18 +1,19 @@
 package com.inventory.inventory.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "products")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
