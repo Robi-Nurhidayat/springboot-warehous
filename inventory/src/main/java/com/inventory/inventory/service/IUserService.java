2ba9acb65e8aa16107fc4a9e7082ae8a5a3ace2c
Robi Nurhidayat <robivanpersie290899@gmail.com>
2024-08-21 08:58:11 +0700

    create userconstant,repository,productservice interface

---
+++ inventory/src/main/java/com/inventory/inventory/service/IUserService.java
@@ -0,0 +1,17 @@
+package com.inventory.inventory.service;
+
+import com.inventory.inventory.entity.User;
+
+import java.util.List;
+
+public interface IUserService {
+
+    List<User> getAllUsers();
+    User findById(Long userId);
+    User save(User user);
+    User updateUser(User user);
+    boolean deleteUser(Long userId);
+
+
+
+}
